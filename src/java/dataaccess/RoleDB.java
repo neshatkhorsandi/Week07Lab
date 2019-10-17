023a0c7949a513acc87c9dd24521b681cfc46c1f
784789 <784789@elf8it221373.ACDM.DS.SAIT.CA>
2019-10-17 16:57:22 -0600

    insert, delete and update methods in RoleDB class created

--- src/java/dataaccess/RoleDB.java
+++ src/java/dataaccess/RoleDB.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import models.Role;
 import models.User;
 
@@ -17,6 +19,7 @@ import models.User;
  * @author awarsyle
  */
 public class RoleDB {
+
     public Role getRole(int roleID) throws SQLException {
 
         ConnectionPool connectionPool = null;
@@ -41,7 +44,102 @@ public class RoleDB {
             connectionPool.freeConnection(connection);
         }
     }
-    
- public boolean 
 
+    public boolean delete(Role role) throws SQLException {
+        ConnectionPool connectionPool = null;
+        Connection connection = null;
+
+        try {
+            connectionPool = ConnectionPool.getInstance();
+
+            connection = connectionPool.getConnection();
+
+            String DELETE_STMT = "DELETE FROM role_table where RoleId = ?";
+            PreparedStatement prepare = connection.prepareStatement(DELETE_STMT);
+            prepare.setInt(1, role.getRoleID());
+
+            int rowCount = prepare.executeUpdate();
+            prepare.close();
+            return rowCount == 1;
+        } finally {
+            connectionPool.freeConnection(connection);
+        }
+    }
+
+    public boolean update(Role role) throws SQLException {
+        ConnectionPool connectionPool = null;
+        Connection connection = null;
+
+        try {
+            connectionPool = ConnectionPool.getInstance();
+            connection = connectionPool.getConnection();
+
+            String UPDATE_STMT = "UPDATE role_table set "
+                    + "RoleID = ?, RoleName = ? where RoleID = ?";
+
+            int rowCount = 0;
+            PreparedStatement prepare = connection.prepareStatement(UPDATE_STMT);
+            prepare.setInt(1, role.getRoleID());
+            prepare.setString(2, role.getRoleName());
+
+            rowCount = prepare.executeUpdate();
+            prepare.close();
+
+            return rowCount == 1;
+
+        } finally {
+            connectionPool.freeConnection(connection);
+        }
+    }
+
+    public boolean insert(Role role) throws SQLException {
+
+        ConnectionPool connectionPool = null;
+        Connection connection = null;
+
+        try {
+            int rowCount = 0;
+            connectionPool = ConnectionPool.getInstance();
+            connection = connectionPool.getConnection();
+            String INSERT_STMT = "INSERT INTO role_table"
+                    + "(RoleID, RoleName) VALUES (?,?)";
+
+            PreparedStatement prepare = connection.prepareStatement(INSERT_STMT);
+
+            prepare.setInt(1, role.getRoleID());
+            prepare.setString(2, role.getRoleName());
+
+            return rowCount == 1;
+        } finally {
+            connectionPool.freeConnection(connection);
+        }
+    }
+
+    public List<Role> getAll() throws SQLException {
+        ConnectionPool connectionPool = null;
+        Connection connection = null;
+
+        try {
+            connectionPool = ConnectionPool.getInstance();
+            connection = connectionPool.getConnection();
+
+            Role role;
+            ArrayList<Role> roleList = new ArrayList<>();
+
+            String statement = "SELECT RoleID, RoleName FROM role_tabe";
+            PreparedStatement prepare = connection.prepareStatement(statement);
+            ResultSet resultSet = prepare.executeQuery();
+
+            while (resultSet.next()) {
+                int RoleID = resultSet.getInt(1);
+                String RoleName = resultSet.getString(2);
+
+                role = new Role(RoleID, RoleName);
+                roleList.add(role);
+            }
+            return roleList;
+        } finally {
+            connectionPool.freeConnection(connection);
+        }
+    }
 }
