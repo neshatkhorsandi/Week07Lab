7bdfd3f7d478359f548e14edbb0e5d36612bbc7d
784789 <784789@elf8it221373.ACDM.DS.SAIT.CA>
2019-10-29 09:27:46 -0600

    fixed adding user to db

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -8,139 +8,151 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import models.Role;
 import models.User;
+import services.RoleService;
 import services.UserService;
 
 public class UserServlet extends HttpServlet {
 
-  /**
-   * The doGet method in UserServlet, which works with users.jsp
-   *
-   * @param request request
-   * @param response response
-   * @throws ServletException Servlet Exception
-   * @throws IOException Input/Output Exception
-   */
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    UserService us = new UserService();
+    /**
+     * The doGet method in UserServlet, which works with users.jsp
+     *
+     * @param request request
+     * @param response response
+     * @throws ServletException Servlet Exception
+     * @throws IOException Input/Output Exception
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        UserService us = new UserService();
+        RoleService rs = new RoleService();
 
-    String email = request.getParameter("email");
+        String email = request.getParameter("email");
 
-    String action = request.getParameter("action");
-    action = action == null ? "" : action;
+        String action = request.getParameter("action");
+        action = action == null ? "" : action;
 
-    switch (action) {
-      case "clearEdit": {
-        request.setAttribute("edit", false);
-        request.setAttribute("user", null);
-        break;
-      }
-      case "edit": {
-        if (checkIsValid(new String[]{email})) {
-          request.setAttribute("edit", true);
-          try {
-            request.setAttribute("user", us.get(email));
-          } catch (Exception e) {
-            request.setAttribute("error", "Could not retrieve user.");
-          }
-        } else {
-          request.setAttribute("error", "Could not retrieve user.");
-          return;
+        switch (action) {
+            case "clearEdit": {
+                request.setAttribute("edit", false);
+                request.setAttribute("user", null);
+                break;
+            }
+            case "edit": {
+                if (checkIsValid(new String[]{email})) {
+                    request.setAttribute("edit", true);
+                    try {
+                        request.setAttribute("user", us.get(email));
+                    } catch (Exception e) {
+                        request.setAttribute("error", "Could not retrieve user.");
+                    }
+                } else {
+                    request.setAttribute("error", "Could not retrieve user.");
+                    return;
+                }
+                break;
+            }
+            case "delete": {
+                if (checkIsValid(new String[]{email})) {
+                    try {
+                        us.delete(email);
+                    } catch (Exception e) {
+                        request.setAttribute("error", "Could not delete user.");
+                    }
+                } else {
+                    request.setAttribute("error", "Could not delete user.");
+                    return;
+                }
+                break;
+            }
         }
-        break;
-      }
-      case "delete": {
-        if (checkIsValid(new String[]{email})) {
-          try {
-            us.delete(email);
-          } catch (Exception e) {
-            request.setAttribute("error", "Could not delete user.");
-          }
-        } else {
-          request.setAttribute("error", "Could not delete user.");
-          return;
+
+        try {
+            request.setAttribute("users", us.getAll());
+            request.setAttribute("roles", rs.getAll());
+        } catch (Exception e) {
+            request.setAttribute("error", e.getMessage());
         }
-        break;
-      }
+
+        getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").
+                forward(request, response);
     }
 
-    try {
-      request.setAttribute("users", us.getAll());
-    } catch (Exception e) {
-      request.setAttribute("error", e.getMessage());
+    /**
+     * The doPost method in UserServlet, which works with users.jsp
+     *
+     * @param request request
+     * @param response response
+     * @throws ServletException Servlet Exception
+     * @throws IOException IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        UserService us = new UserService();
+        RoleService rs = new RoleService();
+
+        String email = request.getParameter("email");
+        String fname = request.getParameter("fname");
+        String lname = request.getParameter("lname");
+        String password = request.getParameter("password");
+        int roleValue = Integer.parseInt(request.getParameter("roleValue"));
+
+        String action = request.getParameter("action");
+        action = action == null ? "" : action;
+
+        try {
+            switch (action) {
+                case "add":
+                    if (checkIsValid(new String[]{email, fname, lname,
+                        password})) {
+
+                        us.insert(email, fname, lname, password, roleValue);
+
+                    } else {
+                        request.setAttribute("error", "All fields are required");
+                    }
+                    break;
+                case "edit":
+                    if (checkIsValid(new String[]{email, fname, lname})) {
+
+                        us.update(email, fname, lname, password, roleValue);
+                    } else {
+                        request.setAttribute("error", "All fields are required");
+                    }
+                    break;
+            }
+        } catch (Exception ex) {
+            request.setAttribute("error", ex.getMessage());
+        }
+
+        try {
+            request.setAttribute("users", us.getAll());
+            request.setAttribute("roles", rs.getAll());
+
+        } catch (Exception ex) {
+            request.setAttribute("error", ex.getMessage());
+        }
+
+        getServletContext().getRequestDispatcher("/WEB-INF/users.jsp")
+                .forward(request, response);
     }
 
-    getServletContext().getRequestDispatcher("/WEB-INF/users.jsp").forward(request, response);
-  }
-
-  /**
-   * The doPost method in UserServlet, which works with users.jsp
-   *
-   * @param request request
-   * @param response response
-   * @throws ServletException Servlet Exception
-   * @throws IOException IOException
-   */
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-          throws ServletException, IOException {
-    UserService us = new UserService();
-
-    String email = request.getParameter("email");
-    String fname = request.getParameter("fname");
-    String lname = request.getParameter("lname");
-    String password = request.getParameter("password");
-    String roleString = request.getParameter("roles");
-    Role role = new Role();
-    //Role role = request.getParameter("role");
-    //get role from roleID
-    String action = request.getParameter("action");
-    action = action == null ? "" : action;
-
-    try {
-      switch (action) {
-        case "add":
-          if (checkIsValid(new String[]{email, fname, lname, password,role.getRoleName()})) {
-            us.insert(email, fname, lname, password, role.getRoleID());
-          } else {
-            request.setAttribute("error", "All fields are required");
-          }
-        case "edit":
-          if (checkIsValid(new String[]{email, fname, lname, role.getRoleName()})) {
-            us.update(email, fname, lname, password, role.getRoleID());
-          } else {
-            request.setAttribute("error", "All fields are required");
-          }
-      }
-    } catch (Exception ex) {
-      request.setAttribute("error", ex.getMessage());
+    /**
+     * Goes through values and ensures they are all not null or empty
+     *
+     * @param values values
+     * @return boolean if all values are valid
+     */
+    private boolean checkIsValid(String[] values) {
+        // check each elemenet in array for null or empty string
+        // return false if one is found
+        for (String s : values) {
+            if (s == null || s.equals("")) {
+                return false;
+            }
+        }
+        return true;
     }
-
-    try {
-      request.setAttribute("users", us.getAll());
-    } catch (Exception ex) {
-      request.setAttribute("error", ex.getMessage());
-    }
-
-    getServletContext().getRequestDispatcher("/WEB-INF/users.jsp")
-            .forward(request, response);
-  }
-
-  /**
-   * Goes through values and ensures they are all not null or empty
-   *
-   * @param values values
-   * @return boolean if all values are valid
-   */
-  private boolean checkIsValid(String[] values) {
-    // check each elemenet in array for null or empty string
-    // return false if one is found
-    for (String s : values) {
-      if (s == null || s.equals("")) {
-        return false;
-      }
-    }
-    return true;
-  }
 }
